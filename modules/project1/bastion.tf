64030736605710b486c9c887fd017a08d451b521
teju488407 <teju488407@gmail.com>
2019-07-25 15:46:58 +0530

    --- Auto-Git Commit ---

--- modules/project1/bastion.tf
+++ modules/project1/bastion.tf
@@ -1,10 +1,9 @@
 resource "google_compute_instance" "default" {
-  name         = "test"
+  name         = "bastion-pro1"
   machine_type = "n1-standard-1"
   zone         = "us-central1-f"
 
   tags = ["bastion-ssh"]
-
   boot_disk {
     initialize_params {
       image = "centos-cloud/centos-7"
