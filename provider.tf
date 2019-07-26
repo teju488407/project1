4cae41f622d89cf2869878af7966fe22bec92238
teju488407 <teju488407@gmail.com>
2019-07-26 19:32:39 +0530

    Terraform changes

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "google" {
-  credentials = "creds.json"
+  credentials = credentials('terraform-auth')//"creds.json"
   project     = "${var.project-id}"
   region      = "${var.region}"
   //org_id     = "1052607630679"
