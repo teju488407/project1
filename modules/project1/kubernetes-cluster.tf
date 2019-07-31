28cfa4b506f36897fa7f2b60c544c0e0019430ca
teju488407 <teju488407@gmail.com>
2019-07-31 19:12:25 +0530

    --- Auto-Git Commit ---

--- modules/project1/kubernetes-cluster.tf
+++ modules/project1/kubernetes-cluster.tf
@@ -8,6 +8,10 @@ resource "google_container_cluster" "primary" {
   network_policy { 
       enabled = true
   }
+  private_cluster_config{
+    enable_private_nodes = true
+    enable_private_endpoint = true
+  }
 }
 
 resource "google_container_node_pool" "my-pool" {
